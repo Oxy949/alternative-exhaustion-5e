900e736c09c17682f8afd7463a60b4f6bbed06cb
Konstantin <oxy949@gmail.com>
2025-05-08 21:35:31 +0300

    Update setup.mjs

--- module/setup.mjs
+++ module/setup.mjs
@@ -1,14 +1,11 @@
 Hooks.once("init", function() {
   const path = "modules/alternative-exhaustion-5e/module/assets/exhaustion";
   const uuid = "Compendium.alternative-exhaustion-5e.exhaustion.JournalEntry.lMTExo0cxBVCDIgF.JournalEntryPage.cspWveykstnu3Zcv";
-  CONFIG.DND5E.conditionEffects.halfHealth.delete("exhaustion-4");
-  CONFIG.DND5E.conditionEffects.halfMovement.delete("exhaustion-2");
-  CONFIG.DND5E.conditionEffects.noMovement.delete("exhaustion-5");
   CONFIG.DND5E.conditionTypes.exhaustion.levels = 10;
-  CONFIG.DND5E.conditionTypes.exhaustion.icon = `${path}.svg`;
+  CONFIG.DND5E.conditionTypes.exhaustion.img = `${path}.svg`;
   CONFIG.DND5E.conditionTypes.exhaustion.reference = uuid;
-  const exh = CONFIG.statusEffects.find(e => e.id === "exhaustion");
-  foundry.utils.mergeObject(exh, CONFIG.DND5E.conditionTypes.exhaustion, {insertKeys: false});
+  CONFIG.DND5E.conditionTypes.exhaustion.reduction.rolls = 0;
+  CONFIG.DND5E.conditionTypes.exhaustion.reduction.speed = 0;
 
   game.settings.register("alternative-exhaustion-5e", "manual", {
     name: "ALT_EXH.SETTING_MANUAL",
@@ -20,25 +17,31 @@ Hooks.once("init", function() {
     requiresReload: true
   });
 
-  if (game.settings.get("alternative-exhaustion-5e", "manual")) return;
+  if (game.settings.get("alternative-exhaustion-5e", "manual"))
+    return;
 
   Hooks.on("dnd5e.preRollDeathSave", function(actor, config) {
     const exh = actor.system.attributes?.exhaustion ?? 0;
-    if (exh > 0) config.parts.push("-@attributes.exhaustion");
+    if (exh > 0) 
+      config.parts.push("-@attributes.exhaustion");
   });
 
   Hooks.on("dnd5e.prepareLeveledSlots", function(spells, actor, progression) {
     const exh = actor.system.attributes?.exhaustion ?? 0;
-    if (!(exh > 0)) return;
+    if (!(exh > 0)) 
+      return;
+
     for (const [k, v] of Object.entries(actor.system.skills ?? {})) {
       v.total -= exh;
       v.passive -= exh;
     }
+
     for (const [k, v] of Object.entries(actor.system.abilities ?? {})) {
       v.mod -= exh;
       v.save -= exh;
       v.dc -= exh;
     }
+    
     actor.system.attributes.spelldc -= exh;
   });
 });
